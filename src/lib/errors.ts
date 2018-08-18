0a5013ac1cefb97ef0e2c3a942185a6a09b85ce2
Lenglet Anthony <anthony-62100@hotmail.fr>
2018-08-18 02:58:33 +0200

    ✨ Added NoCardError, LinkError, PPUStartupError and CPUStartupError

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -11,3 +11,31 @@ export class InstructionError extends Error {
     this.name = 'InstructionError'
   }
 }
+
+export class NoCardError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'NoCardError'
+  }
+}
+
+export class LinkError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'LinkError'
+  }
+}
+
+export class PPUStartupError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'PPUStartupError'
+  }
+}
+
+export class CPUStartupError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'CPUStartupError'
+  }
+}
