91696bfffdbf1bad0ecbffb1fc37c0c3e3fc52b8
Lenglet Anthony <anthony-62100@hotmail.fr>
2018-08-27 19:54:58 +0200

    ✨ Added error and warning message types to clog

--- src/lib/clog.ts
+++ src/lib/clog.ts
@@ -38,4 +38,41 @@ export class clog {
       this.prefix ? console.log(this.prefix, message) : console.log(message)
     }
   }
+
+  /**
+   * Log an error message to the console
+   * @param {string} type the type of the error message
+   * @param {?[]} message the error to be outputted to the console
+   */
+  error(type: string, ...message: any[]): void {
+    if (isGlobalDevMode || this.isdevmode) {
+      if (this.prefix) {
+        console.error(this.prefix, type + ':', ...message)
+      } else {
+        console.error(type + ':', ...message)
+      }
+    }
+  }
+
+  /**
+   * Log a warning message to the console
+   * @param {?[]} message the warning to be outputted to the console
+   */
+  warning(...message: any[]): void {
+    if (isGlobalDevMode || this.isdevmode) {
+      if (this.prefix) { message.unshift(this.prefix) }
+      console.warn(...message)
+    }
+  }
+
+  /**
+   * Log an info message to the console
+   * @param {?[]} message the message to be outputted to the console
+   */
+  // info(...message: any[]): void {
+  //   if (isGlobalDevMode || this.isdevmode) {
+  //     if (this.prefix) { message.unshift(this.prefix) }
+  //     console.info(...message)
+  //   }
+  // }
 }
